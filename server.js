ff6bf9d5fa703856ea07875455a45e65d1990437
IVONEI JUNIOR <ivoneijr@gmail.com>
2017-01-27 11:23:15 -0200

    first changes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+import path from 'path';
 var express = require('express');
-var app = express();
+
+let app = express();
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
@@ -11,8 +13,12 @@ app.set('view engine', 'ejs');
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
+app.get('/ping', (req, res) => {
+  res.send({status: 'ok'});
+});
+
 // set the home page route
-app.get('/', function(req, res) {
+app.get('/*', function(req, res) {
 
 	// ejs render automatically looks in the views folder
 	res.render('index');
